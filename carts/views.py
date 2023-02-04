83dcd4c31aca1e0da7dc4186154e8edea8d1d972
Tung Huynh <mtl0612@gmail.com>
2023-02-05 00:31:36 +0700

    Modify template

--- carts/views.py
+++ carts/views.py
@@ -163,8 +163,6 @@ def remove_cart_item(request, product_id, cart_item_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
-        tax = 0
-        grand_total = 0
         if request.user.is_authenticated:
             cart_items = CartItem.objects.filter(user=request.user, is_active=True)
         else:
@@ -173,8 +171,6 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-        tax = (2 * total)/100
-        grand_total = total + tax
     except ObjectDoesNotExist:
         pass #just ignore
 
@@ -182,8 +178,6 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
-        'tax'       : tax,
-        'grand_total': grand_total,
     }
     return render(request, 'store/cart.html', context)
 
@@ -191,8 +185,6 @@ def cart(request, total=0, quantity=0, cart_items=None):
 @login_required(login_url='login')
 def checkout(request, total=0, quantity=0, cart_items=None):
     try:
-        tax = 0
-        grand_total = 0
         if request.user.is_authenticated:
             cart_items = CartItem.objects.filter(user=request.user, is_active=True)
         else:
@@ -201,8 +193,6 @@ def checkout(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-        tax = (2 * total)/100
-        grand_total = total + tax
     except ObjectDoesNotExist:
         pass #just ignore
 
@@ -210,7 +200,5 @@ def checkout(request, total=0, quantity=0, cart_items=None):
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
-        'tax'       : tax,
-        'grand_total': grand_total,
     }
     return render(request, 'store/checkout.html', context)
--- orders/views.py
+++ orders/views.py
@@ -83,13 +83,9 @@ def place_order(request, total=0, quantity=0,):
     if cart_count <= 0:
         return redirect('store')
 
-    grand_total = 0
-    tax = 0
     for cart_item in cart_items:
         total += (cart_item.product.price * cart_item.quantity)
         quantity += cart_item.quantity
-    tax = (2 * total)/100
-    grand_total = total + tax
 
     if request.method == 'POST':
         form = OrderForm(request.POST)
@@ -107,8 +103,6 @@ def place_order(request, total=0, quantity=0,):
             data.state = form.cleaned_data['state']
             data.city = form.cleaned_data['city']
             data.order_note = form.cleaned_data['order_note']
-            data.order_total = grand_total
-            data.tax = tax
             data.ip = request.META.get('REMOTE_ADDR')
             data.save()
             # Generate order number
@@ -126,8 +120,6 @@ def place_order(request, total=0, quantity=0,):
                 'order': order,
                 'cart_items': cart_items,
                 'total': total,
-                'tax': tax,
-                'grand_total': grand_total,
             }
             return render(request, 'orders/payments.html', context)
     else:
